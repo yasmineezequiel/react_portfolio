236b08035485f12c69ce037294bc0dd1e15639e2
yasmine <yasmine.ezequiel@gmail.com>
2019-09-17 16:16:33 +0200

    Adds styling and image

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import axios from "axios"
 import ProjectCard from "./ProjectCard"
+import { UndrawDesigner } from 'react-undraw-illustrations';
 
 
 class Projects extends Component {
@@ -34,12 +35,19 @@ class Projects extends Component {
     }
 
     return (
-
       <div className="ui main container">
-        <h1 className="ui header">My Projects</h1>
+        <div className="ui stackable two column grid">
+          <div className="column">
+          <UndrawDesigner/>
+          </div>
+          <div className="column">
+            <h1 className="ui header">My Projects</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad iusto sequi modi totam qui veniam? Ab asperiores inventore distinctio.</p>
+          </div>
+        </div>
         <div className="ui stackable four column grid">
           {projectsList}
-      </div>
+        </div>
       </div>
     )
   }
