475d221bb8538e47339593f4ad63e5a90a6ae939
yasmine <yasmine.ezequiel@gmail.com>
2019-09-17 10:34:24 +0200

    Adds jsx header, footer, webpack and index.html

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Hello from "./Hello"
+import Header from "./Header"
+import Footer from "./Footer"
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hello />
+      <Footer />
+    </div >
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById("app"))
